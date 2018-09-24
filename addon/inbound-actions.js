d8e9a25132e8b9520b2d6a725dc3445801f32e95
Omair Vaiyani <omairvaiyani@users.noreply.github.com>
2018-09-24 12:57:40 +0100

    Replace Ember import with specific modules

--- addon/inbound-actions.js
+++ addon/inbound-actions.js
@@ -1,10 +1,12 @@
-import Em from 'ember';
 import ActionProxy from './action-proxy';
+import { on } from "@ember/object/evented";
+import Mixin from '@ember/object/mixin';
+import { run } from "@ember/runloop";
 
-let scheduleInAfterRender = /^1\.13|^[2-9]/.test(Em.VERSION);
+let scheduleInAfterRender = /^1\.13|^[2-9]/.test(Ember.VERSION);
 
-export default Em.Mixin.create({
-  _inbound_actions_setup: Em.on('init', function() {
+export default Mixin.create({
+  _inbound_actions_setup: on('init', function() {
     this._inbound_actions_maybeScheduleInAfterRender(() => {
       var proxy = ActionProxy.create({ target: this });
       this.set('actionReceiver', proxy);
@@ -12,12 +14,12 @@ export default Em.Mixin.create({
   }),
   _inbound_actions_maybeScheduleInAfterRender: function(fn) {
     if (scheduleInAfterRender) {
-      Em.run.schedule('afterRender', this, fn);
+      run.schedule('afterRender', this, fn);
     } else {
       fn();
     }
   },
-  onDestroy: Em.on('willDestroyElement', function() {
+  onDestroy: on('willDestroyElement', function() {
     this.set('actionReceiver', null);
   })
 });
